5a7002a042883c494008c689be66d79dcac59383
Bruna Feijó <brunarbfeijo@hotmail.com>
2019-04-01 20:24:19 -0300

    npm run build

--- src/data/careerProfile.js
+++
@@ -1,7 +0,0 @@
-export const careerProfile = {
-  sectionTitle: 'Perfil Profissional',
-  icon: null,
-  description: '<p>Possuo bacharelado em Comunicação Social - Jornalismo, com experiência em televisão, jornal impresso e produção jornalistica online.</p> <p><a href="https://www.facebook.com/bruna.feijo.3/videos/vb.100002009130875/667476653329300/?type=3" target="about_blank">Portfólio</>'
-};
-
-export default careerProfile;
