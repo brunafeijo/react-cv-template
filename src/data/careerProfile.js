7203420bc7fc7a9800fe4c5d007d03f343e35a29
Bruna Feijó <brunarbfeijo@hotmail.com>
2019-04-06 18:21:50 -0300

    Adicao do link do portfolio

--- src/data/careerProfile.js
+++ src/data/careerProfile.js
@@ -1,7 +1,7 @@
 export const careerProfile = {
   sectionTitle: 'Perfil Profissional',
   icon: null,
-  description: '<p>Possuo bacharelado em Comunicação Social - Jornalismo, com experiência em televisão, jornal impresso e produção jornalística online.</p> <p>Acesse meu <a href="https://www.facebook.com/bruna.feijo.3/videos/vb.100002009130875/667476653329300/?type=3" target="about_blank"> portfólio.</p>'
+  description: '<p>Possuo bacharelado em Comunicação Social - Jornalismo, com experiência em televisão, jornal impresso e produção jornalística online.</p> <p>Acesse meu <a href="https://youtu.be/RvpQ_QUeUa4" target="about_blank"> portfólio.</p>'
 };
 
 export default careerProfile;
